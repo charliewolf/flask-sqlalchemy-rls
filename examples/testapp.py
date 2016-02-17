7e1fa66df53f16386d6ca45e3b25ac46e0fdb604
Charlie Wolf <charlie@wolf.is>
2016-02-17 15:42:55 -0500

    clean up and pep8-ize

--- examples/testapp.py
+++ examples/testapp.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcdefg'
 db = SQLAlchemy(app)
 
+
 class TestModel(db.Model):
-   id = Column(Integer, primary_key=True)
-   name = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
 
 class RegisteredUser(UserMixin):
     get_id = lambda self: 1
@@ -19,19 +21,22 @@ registered_user = RegisteredUser()
 
 login_manager = LoginManager(app)
 login_manager.anonymous_user = AnonymousUser
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return registered_user
-
 
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 @app.route('/')
 def get_all():
-   return str(TestModel.query.all())
+    return str(TestModel.query.all())
+
 
 @app.route("/logout")
 @login_required
@@ -39,9 +44,8 @@ def logout():
     logout_user()
     return redirect('/')
 
+
 @app.route("/login")
 def login():
     login_user(registered_user)
     return redirect('/')
-    
-
--- flask_sqlalchemy_rls/__init__.py
+++ flask_sqlalchemy_rls/__init__.py
@@ -4,9 +4,12 @@ import flask_login
 
 SET_ROLE_SQL = sqlalchemy.sql.text("SET LOCAL ROLE :role_name;")
 
+
 class AnonymousUser(flask_login.AnonymousUserMixin):
+
     def get_sql_role(self):
         return 'anonymous'
+
 
 class RowLevelAuthSession(flask_sqlalchemy.SignallingSession):
 
@@ -15,9 +18,11 @@ class RowLevelAuthSession(flask_sqlalchemy.SignallingSession):
         sqlalchemy.event.listen(self, 'after_begin', self.set_local_role)
 
     def set_local_role(self, session, txn, connection):
-        connection.execute(SET_ROLE_SQL, role_name=flask_login.current_user.get_sql_role())
+        connection.execute(
+            SET_ROLE_SQL, role_name=flask_login.current_user.get_sql_role())
 
 
 class SQLAlchemy(flask_sqlalchemy.SQLAlchemy):
+
     def create_session(self, options):
         return RowLevelAuthSession(self, **options)
